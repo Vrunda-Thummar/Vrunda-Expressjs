5352ff2a19e8bca2c5c9e915885b3cc14a8080aa
Vrunda-Thummar <vrundathummar51@gmail.com>
2024-08-08 18:42:56 +0530

    08-08-2024

---
+++ server.js
@@ -0,0 +1,71 @@
+// ------------------------------------------- lec_1
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome to Express');
+//     res.status(200);
+//     res.end();
+// });
+// app.get('/login', (req, res) => {
+//     res.json({ msg: 'Welcome to Login Page' });
+//     res.end();
+// });
+// app.post('/login', (req, res) => {
+//     res.send('Welcome to Login Post method');
+//     res.end();
+// });
+// app.listen(2304, () => {
+//     console.log('Server is connected at http://localhost:2304');
+// });
+
+
+
+// ----------------------------------------------- lec_2
+
+const express = require('express');
+const app = express();
+// const data = require('./friend.json');
+const fs = require('fs');
+const data = fs.readFilesync('./friend.json', 'utf-8');
+// console.log(data);
+
+// POST, GET, PUT, PATCH, DELETE
+Server.get("/", (req,res)=>{
+    res.write('Welcome to Express Server');
+    res.end();
+})
+
+// Server.get("/", (req,res)=>{
+//     res.write('GET Method - 1');
+//     res.end();
+// })
+
+Server.post("/", (req,res)=>{
+    // res.write('Welcome to Post Method');
+    res.send('<h1>Send Method</h1>');
+})
+
+server.put("/", (req,res)=>{
+    res.json({message: 'Hello'});
+})
+
+server.patch("/", (res,req)=>{
+    res.status(400);
+    res.json({message:'Hello'})
+})
+
+server.get("/user", (req,res)=>{
+    res.json(json.parse(data));
+})
+
+serever.get("/login", (req,res)=>{
+    res.write('Welcome to Login Page');
+    res.end();
+})
+
+server.listen(2304, ()=>{
+    console.log('Server Start at http://localhost:2304');   
+})
+
