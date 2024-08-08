a1524d2ef705069fde694107f33d70afd62d78bd
Vrunda-Thummar <vrundathummar51@gmail.com>
2024-08-08 10:25:51 +0530

    08-08-2024

--- lec_1.js
+++ lec_1.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Express');
+    res.status(200);
+    res.end();
+});
+app.get('/login', (req, res) => {
+    res.json({ msg: 'Welcome to Login Page' });
+    res.end();
+});
+app.post('/login', (req, res) => {
+    res.send('Welcome to Login Post method');
+    res.end();
+});
+app.listen(8000, () => {
+    console.log('Server is connected at port 8000');
+});
