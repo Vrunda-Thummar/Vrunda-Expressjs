a888a473be9dcfe2a9ff3fc736b3e0ed651771e7
Vrunda-Thummar <vrundathummar51@gmail.com>
2024-08-09 17:50:44 +0530

    09-08-2024

--- server.js
+++ server.js
@@ -24,48 +24,94 @@
 
 // ----------------------------------------------- lec_2
 
-const express = require('express');
-const app = express();
-// const data = require('./friend.json');
-const fs = require('fs');
-const data = fs.readFilesync('./friend.json', 'utf-8');
-// console.log(data);
+// const express = require('express');
+// const app = express();
+// // const data = require('./friend.json');
+// const fs = require('fs');
+// const data = fs.readFilesync('./friend.json', 'utf-8');
+// // console.log(data);
 
-// POST, GET, PUT, PATCH, DELETE
-Server.get("/", (req,res)=>{
-    res.write('Welcome to Express Server');
-    res.end();
-})
-
+// // POST, GET, PUT, PATCH, DELETE
 // Server.get("/", (req,res)=>{
-//     res.write('GET Method - 1');
+//     res.write('Welcome to Express Server');
 //     res.end();
 // })
 
-Server.post("/", (req,res)=>{
-    // res.write('Welcome to Post Method');
-    res.send('<h1>Send Method</h1>');
-})
+// // Server.get("/", (req,res)=>{
+// //     res.write('GET Method - 1');
+// //     res.end();
+// // })
 
-server.put("/", (req,res)=>{
-    res.json({message: 'Hello'});
-})
+// Server.post("/", (req,res)=>{
+//     // res.write('Welcome to Post Method');
+//     res.send('<h1>Send Method</h1>');
+// })
 
-server.patch("/", (res,req)=>{
-    res.status(400);
-    res.json({message:'Hello'})
-})
+// server.put("/", (req,res)=>{
+//     res.json({message: 'Hello'});
+// })
 
-server.get("/user", (req,res)=>{
-    res.json(json.parse(data));
-})
+// server.patch("/", (res,req)=>{
+//     res.status(400);
+//     res.json({message:'Hello'})
+// })
 
-serever.get("/login", (req,res)=>{
-    res.write('Welcome to Login Page');
-    res.end();
-})
+// server.get("/user", (req,res)=>{
+//     res.json(json.parse(data));
+// })
 
-server.listen(2304, ()=>{
-    console.log('Server Start at http://localhost:2304');   
-})
+// serever.get("/login", (req,res)=>{
+//     res.write('Welcome to Login Page');
+//     res.end();
+// })
 
+// server.listen(2304, ()=>{
+//     console.log('Server Start at http://localhost:2304');   
+// })
+
+
+
+// ------------------------------------------- lec_3 (Middleware)
+
+// const express = require('express');
+// const app = express();
+// const data = require('./friend.json');
+// const fs = require('fs');
+// const data = fs.readFilesync('./friend.json', 'utf-8');
+// console.log(data);
+
+// const Middleware = (req, res, next) => {
+//     // console.log(req.query);
+//     if(req.query.age >= "19")
+//     {
+//         console.log('Success');
+//         next();        
+//     }
+//     else
+//     {
+//         res.json({message: "Sorry....."})
+//     }
+    
+// }
+
+// // server.use(Middleware); //application
+
+// // POST, GET, PUT, PATCH, DELETE
+// Server.get("/", (req,res)=>{
+//     res.write('Welcome to Express Server');
+//     res.end();
+// })
+
+// serever.get("/login", Middleware, (req,res)=>{
+//     res.write('Welcome to Login Page');
+//     res.end();
+// })
+
+// Server.post("/", (req,res)=>{
+//     // res.write('Welcome to Post Method');
+//     res.send('<h1>Send Method</h1>');
+// })
+
+// server.listen(2304, () => {
+//     console.log('server start at http://localhost:2304');
+// })
