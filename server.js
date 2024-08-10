02be5908058d52ee6eed2ef3a19f16425f027e17
Vrunda-Thummar <vrundathummar51@gmail.com>
2024-08-10 17:58:02 +0530

    10-08-2024

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 const express = require('express');
 const morgan = require('morgan');
-const server = express;
+const server = express();
 const users = require('./friend.json');
 // console.log(users);
 
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
@@ -17,9 +17,9 @@ server.get("/", (req, res) => {
 // CRUD
 // Create User
 server.post("/user", (req, res) => {
-    // console.log(req.body);
-    users.push(req.body);
-    res.json({ message: "User Added Success" });
+    console.log(req.body);
+    // users.push(req.body);
+    // res.json({ message: "User Added Success" });
 });
 
 // READ - Get All Users
@@ -37,5 +37,3 @@ server.get("/user/:id", (req, res) => {
 server.listen(2304, () => {
     console.log('server start at http://localhost:2304');
 })
-
-
