94b6388c0f7dd10b7fc7915f3a7b879439400e03
Vrunda-Thummar <vrundathummar51@gmail.com>
2024-09-03 16:34:51 +0530

    03-09-2024

---
+++ controller/order.controller.js
@@ -0,0 +1,52 @@
+const Order = require("../model/order.model");
+const Cart = require("../model/cart.model");
+
+exports.allNewOrder = async (req, res) => {
+    try {
+        let carts = await Cart.find({
+            user: req.user.Id,
+            isDelete: false
+        }).populate({ path: "productId" });
+
+        if (carts.length === 0) {
+            return res.json({ message: 'No Cart Found' });
+        }
+
+        let orderItems = carts.map((item) => ({
+            productId: item.productId._id,
+            quantity: item.quantity,
+            price: item.productId.price,
+            totalprice: item.quantity * item.productId.price
+        }))
+        // console.log(orderItems);
+        let amount = orderItems.reduce((total, item) => total += item.totalprice, 0);
+        // console.log(amount);
+
+        let order = await Order.create({
+            user: req.user._id,
+            items: orderItems,
+            paidAmount: amount
+        });
+        await Cart.updateMany({user: req.user._id, isDelete: false}, {isDelete: true});
+        res.json({message: "Order Placed", order});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+// let find = [
+//     {
+//         $match: {user: req.user._id, isDelete: false}
+//     },
+//     {
+//         $lookup:{
+//             from: "products",
+//             localField: "productId",
+//             foreignField: "_id",
+//             as: "product"
+//         }
+//     }
+// ];
+
+// let carts = await Cart.aggregate(find);
